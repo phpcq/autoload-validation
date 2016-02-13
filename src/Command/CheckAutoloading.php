d66c736c89b203e3c6672e89db2fa4b71f91ae14
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-02-13 11:10:00 +0100

    Correct report instantiation

--- src/Command/CheckAutoloading.php
+++ src/Command/CheckAutoloading.php
@@ -26,6 +26,7 @@ use PhpCodeQuality\AutoloadValidation\AutoloadValidator;
 use PhpCodeQuality\AutoloadValidation\AutoloadValidator\AutoloadValidatorFactory;
 use PhpCodeQuality\AutoloadValidation\ClassMapGenerator;
 use PhpCodeQuality\AutoloadValidation\Report\Destination\PsrLogDestination;
+use PhpCodeQuality\AutoloadValidation\Report\Report;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -77,9 +78,8 @@ class CheckAutoloading extends Command
         $report   = new Report($destinations);
         $composer = json_decode(file_get_contents($rootDir . '/composer.json'), true);
         $factory  = new AutoloadValidatorFactory($rootDir, new ClassMapGenerator(), $logger);
-        $test     = new AutoloadValidator($factory->createFromComposerJson($composer), $logger);
-        if (!$test->validate()) {
         $test     = new AutoloadValidator($factory->createFromComposerJson($composer), $report);
+        if (!$test->validate()) {
             return 1;
         }
 
