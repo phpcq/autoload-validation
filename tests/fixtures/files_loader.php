51c7b9b303aeed27db59d2b873b24676a8b95a10
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-02-16 01:02:32 +0100

    Add file header to fixture

--- tests/fixtures/files_loader.php
+++ tests/fixtures/files_loader.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * This file is part of phpcq/autoload-validation.
+ *
+ * (c) 2014 Christian Schiffler, Tristan Lins
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    phpcq/autoload-validation
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2014-2016 Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @license    https://github.com/phpcq/autoload-validation/blob/master/LICENSE MIT
+ * @link       https://github.com/phpcq/autoload-validation
+ * @filesource
+ */
+
 spl_autoload_register(
     function ($class) {
         // No op.
